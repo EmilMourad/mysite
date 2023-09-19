a59907ff14187b145a9476398da1dc1969aa8e6e
Emil <emilmourad8@gmail.com>
2023-09-19 13:52:11 +0300

    added views for blog app

--- blog/models.py
+++ blog/models.py
@@ -2,28 +2,37 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
 
     class Status(models.TextChoices):
-        DRAFT = 'DF' , 'Draft'
-        PUBLISHED = 'PB' , 'Published'
-    
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255)
     body = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
-    published = models.DateTimeField(default=timezone.now)
+    publish = models.DateTimeField(default=timezone.now)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=2 , choices=Status.choices , default=Status.DRAFT)
-    author = models.ForeignKey(User , on_delete=models.CASCADE , related_name='blog_posts')
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='blog_posts')
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
-        ordering = ['-published']
+        ordering = ['-publish']
         indexes = [
-            models.Index(fields=['-published']),
+            models.Index(fields=['-publish']),
         ]
 
     def __str__(self):
         return self.title
-
-
